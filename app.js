7c71a49d0cc72164474c2d9f4d1c0e82b5b01506
Vaibhav Singh <vaibhavdelhi53@gmail.com>
2026-04-18 13:00:55 +0530

    Initial portfolio

---
+++ app.js
@@ -0,0 +1,31 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function toggleDetails(card) {
+  document.querySelectorAll(".card").forEach(c => {
+    if (c !== card) c.classList.remove("active");
+  });
+
+  card.classList.toggle("active");
+}
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const pos = sec.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
+
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(50px)";
+  sec.style.transition = "0.6s";
+});
